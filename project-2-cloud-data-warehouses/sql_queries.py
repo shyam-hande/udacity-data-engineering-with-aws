8c632dfe77c2396b6a175a2b6fbf4b39a78f9802
shyam-hande <shyam.hande@thoughtworks.com>
2025-12-21 09:30:46 +0530

    Refactor SQL queries for clarity and organization

--- project-2-cloud-data-warehouses/sql_queries.py
+++ project-2-cloud-data-warehouses/sql_queries.py
@@ -1,27 +1,28 @@
 import configparser
 
 
-# CONFIG
+# CONFIGURATIONS
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
-ARN = config.get("IAM_ROLE", "ARN")
+# S3 AND ROLE 
+SONG_DATA = config.get("S3", "SONG_DATA")
 LOG_DATA = config.get("S3", "LOG_DATA")
 LOG_JSONPATH = config.get("S3", "LOG_JSONPATH")
-SONG_DATA = config.get("S3", "SONG_DATA")
+ARN = config.get("IAM_ROLE", "ARN")
 
-# DROP TABLES
-
+# DROP TABLES SECTION STAGING
 staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
 staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+
+# DROP TABLES SECTION ANALYTICS
 songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
 user_table_drop = "DROP TABLE IF EXISTS users;"
 song_table_drop = "DROP TABLE IF EXISTS songs;"
 artist_table_drop = "DROP TABLE IF EXISTS artists;"
 time_table_drop = "DROP TABLE IF EXISTS time;"
 
-# CREATE TABLES
-
+# CREATE TABLES SECTION STAGING
 staging_events_table_create = ("""
     CREATE TABLE IF NOT EXISTS staging_events (
         artist              VARCHAR,
@@ -60,6 +61,7 @@ staging_songs_table_create = ("""
     )
 """)
 
+# CREATE TABLES SECTION ANALYTICS
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays (
         songplay_id         INT         IDENTITY(0,1)   PRIMARY KEY,
@@ -116,7 +118,7 @@ time_table_create = ("""
     )
 """)
 
-# STAGING TABLES
+# COPY STAGING TABLES
 
 staging_events_copy = ("""
     copy staging_events 
@@ -201,11 +203,24 @@ time_table_insert = ("""
 """)
 
 # QUERY LISTS
-
-create_table_queries = [staging_events_table_create, staging_songs_table_create,
-                        songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop,
-                      songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert,
-                        song_table_insert, artist_table_insert, time_table_insert]
+drop_table_queries_list = [staging_events_table_drop,
+                      staging_songs_table_drop,
+                      songplay_table_drop,
+                      user_table_drop,
+                      song_table_drop,
+                      artist_table_drop,
+                      time_table_drop]
+create_table_queries_list = [staging_events_table_create,
+                        staging_songs_table_create,
+                        songplay_table_create,
+                        user_table_create,
+                        song_table_create,
+                        artist_table_create,
+                        time_table_create]
+copy_table_queries_list = [staging_events_copy,
+                      staging_songs_copy]
+insert_table_queries_list = [songplay_table_insert,
+                        user_table_insert,
+                        song_table_insert,
+                        artist_table_insert,
+                        time_table_insert]
