5582776dd01135dbc44ecd2aacde4b3f93c13fef
shyam-hande <shyam.hande@thoughtworks.com>
2025-12-21 09:34:51 +0530

    Refactor table creation and dropping functions

--- project-2-cloud-data-warehouses/create_tables.py
+++ project-2-cloud-data-warehouses/create_tables.py
@@ -1,40 +1,30 @@
 import configparser
 import psycopg2
-from sql_queries import create_table_queries, drop_table_queries
+from sql_queries import create_table_queries_list, drop_table_queries_list
 
+# Iterates through the `drop_table_queries_list` and drops each table.
+def drop_tables(cursor, connection):
+    for query in drop_table_queries_list:
+        cursor.execute(query)
+        connection.commit()
 
-def drop_tables(cur, conn):
-    """
-    Drop each table using the queries in `drop_table_queries` list.
-    """
-    for query in drop_table_queries:
-        cur.execute(query)
-        conn.commit()
+# Iterates through the `create_table_queries_list` and create each table.
+def create_tables(cursor, connection):
+    for query in create_table_queries_list:
+        cursor.execute(query)
+        connection.commit()
 
-
-def create_tables(cur, conn):
-    """
-    Create each table i not exists already using the queries in `create_table_queries` list.
-    """
-    for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
-
-
+# Create and drop tables main function - Sparkify DB
 def main():
-    """
-    Main function to create and drop tables for Sparkify database.
-    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
+    connection = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cursor = connection.cursor()
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
+    drop_tables(cursor, connection)
+    create_tables(cursor, connection)
 
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
-
-    conn.close()
+    connection.close()
 
 
 if __name__ == "__main__":
