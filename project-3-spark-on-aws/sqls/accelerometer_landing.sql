aa55dae0b56b059107c8afa9b8aa596642466b3e
shyam-hande <shyam.hande@thoughtworks.com>
2025-12-31 17:05:52 +0530

    Create accelerometer_landing external table

---
+++ project-3-spark-on-aws/sqls/accelerometer_landing.sql
@@ -0,0 +1,16 @@
+CREATE EXTERNAL TABLE `accelerometer_landing`(
+  `user` string COMMENT 'from deserializer', 
+  `timestamp` bigint COMMENT 'from deserializer', 
+  `x` double COMMENT 'from deserializer', 
+  `y` double COMMENT 'from deserializer', 
+  `z` double COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://shyam-hande/accelerometer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
